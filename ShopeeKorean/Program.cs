22d0b1b859ee3cc5b34571606965ec028143ef42
BuiKhanh03 <buikhanh0130@gmail.com>
2025-05-18 21:13:28 +0700

    Setup railway

--- ShopeeKorean/Program.cs
+++ ShopeeKorean/Program.cs
@@ -1,7 +1,8 @@
 using ShopeeKorean.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http//*:{port}");
 // Add services to the container.
 
 builder.Services.AddControllers();
